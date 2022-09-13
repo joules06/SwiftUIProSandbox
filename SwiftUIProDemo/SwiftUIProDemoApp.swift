42a58951bb56936398ea88c6ca373f5c803b0eb6
Julio Rico <jcesar.rico@gmail.com>
2022-09-12 18:55:06 -0600

    Initial Commit

---
+++ SwiftUIProDemo/SwiftUIProDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProDemoApp.swift
+//  SwiftUIProDemo
+//
+//  Created by Julio Rico on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
